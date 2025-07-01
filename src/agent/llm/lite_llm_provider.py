8ee9ccce21bd6c4c70c81ce23e8706a32b4547b3
Felipe Amaral (Compass) <amaral.felipe@live.com>
2025-07-01 07:26:11 -0300

    Created connection to llm into aws bedrock

--- src/agent/llm/lite_llm_provider.py
+++ src/agent/llm/lite_llm_provider.py
@@ -1,9 +1,8 @@
-from langchain_core.runnables import RunnableLambda
 import litellm
+from src.agent.config import LLM_API_KEY, LLM_ENDPOINT, LLM_API_VERSION, LLM_PROVIDER, MODEL, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION
 # litellm._turn_on_debug()
-from src.agent.config import LLM_API_KEY, LLM_ENDPOINT, LLM_API_VERSION, LLM_PROVIDER, MODEL
 
-def get_llm():
+def get_llm_azure():
     def call_llm(prompt):
         response = litellm.completion(
             model=f"{LLM_PROVIDER}/{MODEL}",
@@ -13,5 +12,24 @@ def get_llm():
             api_base=LLM_ENDPOINT,
             api_version=LLM_API_VERSION
         )
+        return response["choices"][0]["message"]["content"]
+    return call_llm
+
+def get_llm_bedrock():
+    def call_llm(prompt):
+        model = "bedrock/amazon.titan-text-lite-v1"
+
+        response = litellm.completion(
+            model=model,
+            messages=[{"role": "user", "content": prompt}],
+            max_tokens=300,
+            temperature=0.7,
+            top_p=0.9,
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            aws_region_name=AWS_DEFAULT_REGION
+        )
+
+        # Exibe a resposta
         return response["choices"][0]["message"]["content"]
     return call_llm
